a04557b1ce84499f7ae77b8964507fa0c30209c1
hypoorr <161346904+hypoorr@users.noreply.github.com>
2024-02-26 19:47:05 +0000

    Delete BreakablesCalculator1.py

--- BreakablesCalculator1.py
+++
@@ -1,9 +0,0 @@
-# each breakable = 0.333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
-
-TotalXP = int(input("How much Total XP needed for next level? "))
-
-print(round(TotalXP / 0.3), "breakables needed for next level")
-
-
-while True:
-    True
